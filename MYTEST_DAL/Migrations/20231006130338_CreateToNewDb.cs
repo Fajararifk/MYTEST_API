ff9c14818f807ed3c2661d23b5db65d72e36973f
Fajar Arif <fajar.ariev@gmail.com>
2023-10-06 20:06:54 +0700

    commit for change name to MYTEST

---
+++ MYTEST_DAL/Migrations/20231006130338_CreateToNewDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MYTEST_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateToNewDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
